019592a0d5e97c9ef976b8cab949a6dfd8a54d7c
paulwinex <paulwinex@gmail.com>
2022-06-19 15:55:39 +0300

    add views

--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from apps.blog.models import Article
 
-# Create your views here.
+
+class IndexView(ListView):
+    template_name = 'blog/index.html'
+    # model = Article
+    queryset = Article.objects.filter(published=True)
+    context_object_name = 'articles'
+
+
+class ArticleView(DetailView):
+    template_name = 'blog/article.html'
+    model = Article
+    context_object_name = 'article'
