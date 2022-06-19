6b6f12222015a35c1c0ceea7286f4c2667292197
paulwinex <paulwinex@gmail.com>
2022-06-19 14:13:49 +0300

    add article model

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=128)
+    banner = models.ImageField(upload_to='articles', blank=True)
+    content = models.TextField(blank=True)
+    author = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    draft = models.BooleanField(default=True, blank=True)
+    published = models.BooleanField(default=True, blank=True)
+
